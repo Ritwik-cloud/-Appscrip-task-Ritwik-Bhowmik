293843be88337a1a3520a867fe83f0215114080c
Ritwik Bhowmik <ritwik.kmg1@gmail.com>
2026-04-13 12:29:53 +0530

    fixed ssl issue

--- api/axios/axios.ts
+++ api/axios/axios.ts
@@ -1,12 +1,14 @@
-
 import axios from "axios";
+import https from 'https';
+
 
 export const baseURL = "https://fakestoreapi.com";
 
 export const AxiosInstance = axios.create({
-  baseURL,
+  baseURL: baseURL,
+
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  }),
 });
-
-
-
 
